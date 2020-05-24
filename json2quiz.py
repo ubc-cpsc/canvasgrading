a8cb3794702d250161146f3c14307f3f083b9f0f
Jonatan Schroeder <jonatan@cs.ubc.ca>
2020-05-24 13:29:25 -0700

    Merged quiz2json and json2quiz into a single script with the ability to sync (push and load) a quiz.

--- json2quiz.py
+++ json2quiz.py
@@ -12,11 +12,21 @@ import argparse
 
 import canvas
 
-MAIN_URL = 'https://canvas.ubc.ca/api/v1'
-
-def end_file(htmlfile):
-    htmlfile.write('</body>\n</html>')
-    htmlfile.close()
+QUIZ_REQ_FIELDS = ['id','title','description','quiz_type',
+                   'assignment_group_id','time_limit','shuffle_answers',
+                   'hide_results','show_correct_answers',
+                   'show_correct_answers_at','hide_correct_answers_at',
+                   'show_correct_answers_last_attempt','allowed_attempts',
+                   'scoring_policy','one_question_at_a_time','cant_go_back',
+                   'access_code','ip_filter','due_at','lock_at','unlock_at',
+                   'published','one_time_results',
+                   'only_visible_to_overrides']
+GROUP_REQ_FIELDS = ['id','name','pick_count','question_points',
+                    'assessment_question_bank_id']
+QUESTION_REQ_FIELDS = ['id','question_name','question_text','quiz_group_id',
+                       'question_type','position','points_possible',
+                       'correct_comments','incorrect_comments',
+                       'neutral_comments','text_after_answers','answers']
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
@@ -24,11 +34,16 @@ group.add_argument("-f", "--canvas-token-file", type=argparse.FileType('r'),
                    help="File containing the Canvas token used for authentication")
 group.add_argument("-t", "--canvas-token",
                    help="Canvas token used for authentication")
-parser.add_argument("input", type=argparse.FileType('r'),
-                    help="Input file")
+parser.add_argument("json_file",
+                    help="JSON file, may be an input file (for pushing), an oautput file (for loading), or both (for sync).")
 parser.add_argument("-c", "--course", type=int, help="Course ID")
 parser.add_argument("-q", "--quiz", type=int, help="Quiz ID")
-parser.add_argument
+parser.add_argument("-p", "--push-quiz", action='store_true',
+                    help="Push JSON values to Canvas")
+parser.add_argument("-l", "--load-quiz", action='store_true',
+                    help="Load JSON from values retrieved from Canvas")
+parser.add_argument("-s", "--strip", action='store_true',
+                    help="Strip from output JSON values that cannot be pushed back in updates.")
 parser.add_argument("-d", "--debug", help="Enable debugging mode",
                     action='store_true')
 args = parser.parse_args()
@@ -38,10 +53,23 @@ if args.canvas_token_file:
     args.canvas_token_file.close()
 canvas = canvas.Canvas(args.canvas_token)
 
-print('Reading JSON file...')
-updated = json.load(args.input)
-if not args.quiz and 'quiz' in updated and 'id' in updated['quiz']:
-    args.quiz = updated['quiz']['id']
+if args.push_quiz and args.load_quiz:
+    json_file = open(args.json_file, mode='r+')
+elif args.push_quiz:
+    json_file = open(args.json_file, mode='r')
+elif args.load_quiz:
+    json_file = open(args.json_file, mode='w')
+else:
+    parser.error('Action missing, must select -p, -l or both.')
+
+if args.push_quiz:
+    print('Reading JSON file...')
+    values_from_json = json.load(json_file)
+    if not args.quiz and 'quiz' in values_from_json and \
+       'id' in values_from_json['quiz']:
+        args.quiz = values_from_json['quiz']['id']
+else:
+    values_from_json = {}    
 
 print('Reading data from Canvas...')
 course = None
@@ -85,36 +113,56 @@ for group_id in [q['quiz_group_id'] for q in questions.values()]:
     if group_id:
         groups[group_id] = canvas.question_group(course, quiz, group_id)
 
-if 'quiz' in updated:
+if 'quiz' in values_from_json:
     print('Pushing updates to quiz...')
-    quiz = canvas.update_quiz(course, quiz['id'], updated['quiz'])
+    quiz = canvas.update_quiz(course, quiz['id'], values_from_json['quiz'])
 
-if 'groups' in updated:
+groups_from_file = {}
+if 'groups' in values_from_json:
     print('Pushing updates to question groups...')
-    for (group_id, group) in updated['groups'].items():
+    for (group_id, group) in values_from_json['groups'].items():
         existing_id = None
         try:
             if int(group_id) in groups: existing_id = int(group_id)
         except:
             pass
-        groups[group_id] = canvas.update_question_group(course, quiz,
-                                                        existing_id,
-                                                        group)
+        group = canvas.update_question_group(course, quiz, existing_id,
+                                             group)['quiz_groups'][0]
+        groups_from_file[group_id] = group
+        groups[group['id']] = group
 
-if 'questions' in updated:
+if 'questions' in values_from_json:
     print('Pushing updates to questions...')
-    for (question_id, question) in updated['questions'].items():
+    for (question_id, question) in values_from_json['questions'].items():
         pass
-        if question['quiz_group_id'] and \
-           str(question['quiz_group_id']) in groups:
-            question['quiz_group_id'] = groups[question['quiz_group_id']]['id']
+        if question['quiz_group_id'] in groups_from_file:
+            question['quiz_group_id'] = groups_from_file[question['quiz_group_id']]['id']
         existing_id = None
         try:
             if int(question_id) in questions: existing_id = int(question_id)
         except:
             pass
-        questions[question_id] = canvas.update_question(course, quiz,
-                                                        existing_id,
-                                                        question)
+        question = canvas.update_question(course, quiz, existing_id, question)
+        questions[question['id']] = question
 
+if args.strip:
+    quiz      = {k:v for k, v in quiz.items()
+                 if k in QUIZ_REQ_FIELDS}
+    groups    = {id:{k:v for k,v in group.items()
+                     if k in GROUP_REQ_FIELDS}
+                 for id, group in groups.items()}
+    questions = {id:{k:v for k,v in question.items()
+                     if k in QUESTION_REQ_FIELDS}
+                 for id, question in questions.items()}
+
+if args.load_quiz:
+    print('Saving values back to JSON file...')
+    json_file.seek(0)
+    json.dump({ 'quiz': quiz,
+                'groups': groups,
+                'questions': questions
+    }, json_file, indent=2, sort_keys=True)
+    json_file.truncate()
+
+json_file.close()
 print('\nDONE.')
